63861004f346505990e83403ac239299e5db3bde
tamilatquest <tamil@quest1.io>
2024-10-17 16:57:52 +0530

    video playing

---
+++ gammalighttherapy/CustomVideoPlayer.swift
@@ -0,0 +1,18 @@
+//
+//  CustomVideoPlayer.swift
+//  gammalighttherapy
+//
+//  Created by Tamilarasan on 17/10/24.
+//
+
+import SwiftUI
+
+struct CustomVideoPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomVideoPlayer()
+}
---
+++ gammalighttherapy/ScreenLightView.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenLightView.swift
+//  gammalighttherapy
+//
+//  Created by Tamilarasan on 17/10/24.
+//
+
+import SwiftUI
+
+struct ScreenLightView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenLightView()
+}
