13573574c0f7194d04260d6be4d7d416a857c763
tamilatquest <tamil@quest1.io>
2024-10-27 14:32:25 +0530

    icon change

--- gammalighttherapy/ContentView.swift
+++ gammalighttherapy/ContentView.swift
@@ -7,31 +7,40 @@ struct FlashingView: View {
     var body: some View {
         ZStack {
             TabView(selection: $selectedTab) {
-                ScreenLightView()
-                    .tabItem {
-                        CustomTabItem(title: "Screen", iconName: "lightbulb.fill")
-                    }
-                
                 FlashLightView()
                     .tabItem {
-                        CustomTabItem(title: "Flash", iconName: "flashlight.on.fill")
+                        CustomTabItem(title: "Flash", iconName: "flashlight.on.fill", isSelected: selectedTab == 1)
+                    }
+                ScreenLightView()
+                    .tabItem {
+                        CustomTabItem(title: "Screen", iconName: "doc.plaintext.fill", isSelected: selectedTab == 0)
                     }
             }
+            .accentColor(.blue)
+            .onAppear(perform: {
+                UITabBar.appearance().unselectedItemTintColor = .darkGray
+            })
         }
+        .background(.blue)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct CustomTabItem: View {
     let title: String
     let iconName: String
+    let isSelected: Bool
     
     var body: some View {
         VStack {
             Image(systemName: iconName)
                 .font(.system(size: 24))
+                .foregroundColor(isSelected ? .white : .blue)
+                .padding(10)
+            
             Text(title)
-                .font(.system(size: 18, weight: .bold))
         }
+        .padding(5)
     }
 }
 
